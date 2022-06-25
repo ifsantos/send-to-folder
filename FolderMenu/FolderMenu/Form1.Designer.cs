62dda80b38411d389f99c078b5847d4d7e1c7f77
Ismael <fs.ismael@gmail.com>
2022-06-25 03:26:09 -0300

    add gitignore

--- FolderMenu/FolderMenu/Form1.Designer.cs
+++ FolderMenu/FolderMenu/Form1.Designer.cs
@@ -29,14 +29,15 @@ namespace FolderMenu
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
             // label1
@@ -47,34 +48,32 @@ namespace FolderMenu
             this.label1.Size = new System.Drawing.Size(50, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 17);
+            this.label2.Location = new System.Drawing.Point(56, 17);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 20);
+            this.label2.Size = new System.Drawing.Size(41, 20);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Root folder";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.Text = "Root";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(111, 14);
+            this.textBox1.Location = new System.Drawing.Point(103, 14);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(370, 27);
-            this.textBox1.TabIndex = 2;
+            this.textBox1.Size = new System.Drawing.Size(378, 27);
+            this.textBox1.TabIndex = 3;
             this.textBox1.Text = "C:\\Music\\Rock\\";
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(111, 47);
+            this.textBox2.Location = new System.Drawing.Point(103, 47);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(270, 27);
-            this.textBox2.TabIndex = 4;
+            this.textBox2.Size = new System.Drawing.Size(278, 27);
+            this.textBox2.TabIndex = 1;
             this.textBox2.Text = "Led Zeppelin";
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb_KeyDown);
             // 
             // label3
             // 
@@ -90,46 +89,47 @@ namespace FolderMenu
             this.button1.Location = new System.Drawing.Point(387, 47);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 5;
+            this.button1.TabIndex = 2;
             this.button1.Text = "Create folder";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(12, 94);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(469, 344);
-            this.listBox1.TabIndex = 6;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(-309, -56);
+            this.label4.Location = new System.Drawing.Point(14, 88);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(172, 20);
+            this.label4.Size = new System.Drawing.Size(131, 20);
             this.label4.TabIndex = 7;
-            this.label4.Text = "Files in containing folder";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
+            this.label4.Text = "Files to new folder";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 111);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(468, 312);
+            this.checkedListBox1.Sorted = true;
+            this.checkedListBox1.TabIndex = 4;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(498, 450);
+            this.ClientSize = new System.Drawing.Size(492, 434);
+            this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Create folder containing selected files...";
+            this.Text = "New folder containing selected files";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -144,8 +144,8 @@ namespace FolderMenu
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
     }
 }
 
--- FolderMenu/FolderMenu/Form1.cs
+++ FolderMenu/FolderMenu/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,44 +13,110 @@ namespace FolderMenu
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        string root;
+        string newFolderName;
+        
+        string[] args;
+        public Form1(string[] args)
         {
             InitializeComponent();
+            this.args = args;
+            this.root = Path.GetDirectoryName(this.args[0]);
+            this.newFolderName = Path.GetFileName(this.args[0]).Split("-")[0].Trim();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            textBox1.Text = this.root;
+            textBox2.Text = this.newFolderName;
+            foreach (var arg in this.args)
+            {
+                Path.GetDirectoryName(arg);
+                checkedListBox1.Items.Add(arg);
+            }
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+               
+            }
+            
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
+            string argList = "";
+            string notCopiedFiles = "";
+            int copiedCounter = 0;
+            int notCopiedCounter = 0;
+            char sep = Path.DirectorySeparatorChar;
+            string destFolder = textBox1.Text + sep + textBox2.Text;
 
+            if (!Directory.Exists(destFolder))
+            {
+                Directory.CreateDirectory(destFolder);
+            }
+            CheckedListBox.ObjectCollection it = checkedListBox1.Items;
+            for (int i = 0; i < it.Count; i++)
+            {
+                if (checkedListBox1.GetItemChecked(i))
+                {
+                    string originFilePath = checkedListBox1.GetItemText(it[i]);
+                    FileAttributes attr = File.GetAttributes(originFilePath);
+
+                    if (attr.HasFlag(FileAttributes.Directory))
+                    {
+                        MessageBox.Show("Its a directory. function to be implemented.");
+                        try
+                        {
+                            Directory.Move(originFilePath, destFolder );
+                            argList += "\n" + destFolder;
+                            copiedCounter++;
+                        }
+                        catch (Exception ex)
+                        {
+                            notCopiedFiles += "\n[" + destFolder + "] " + ex.Message;
+                            notCopiedCounter++;
+                        }
+                    }
+                    else
+                    {
+                        string fileName = Path.GetFileName(originFilePath);
+                        try
+                        {
+                            File.Move(originFilePath, destFolder+sep+ fileName) ;
+                            argList += "\n" + fileName;
+                            copiedCounter++;
+                        } 
+                        catch(Exception ex)
+                        {
+                            notCopiedFiles += "\n["+fileName+"] "+ex.Message ;
+                            notCopiedCounter++;
+                        }
+
+                    }
+                }
+
+            }
+
+            this.Visible = false;
+            DialogResult result = MessageBox.Show(
+                "Files copied: " + copiedCounter + argList + 
+                "\nFiles not copied: " + notCopiedCounter + notCopiedFiles, 
+                "Process finished", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            this.Close();
         }
 
-        private void label4_Click(object sender, EventArgs e)
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+               
+        private void tb_KeyDown(object sender, KeyEventArgs e)
         {
-
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(sender, e);
+            }
         }
     }
 }
